af04d5048d0aefaaccf58f442831711b54efc495
cbkwgl <cbk123@gmail.com>
2022-08-15 16:13:57 +0530

    Add project files.

---
+++ 14CSharpBasics/Classes/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14CSharpBasics.Classes
+{
+    public class BankAccount
+    {
+        private double balance;
+        public double Balance
+        {
+            get { return balance; }
+            protected set
+            {
+                if (value > 0)
+                    balance = value;
+                else
+                    balance = 0;
+            }
+        }
+
+        public BankAccount()
+        {
+            Balance = 71;
+        }
+
+        public BankAccount(double initialBalance)
+        {
+            Balance = initialBalance;
+        }
+
+        //Method Overloading
+        public double AddToBalance(double BalanceToBeAdded)
+        {
+            Balance += BalanceToBeAdded;
+            return Balance;
+        }
+
+        
+    }
+    //Inheritance
+    public class ChildBankAccount : BankAccount
+    {
+        public ChildBankAccount()
+        {
+            Balance = 7;
+        }
+    }
+}
---
+++ 14CSharpBasics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Importing a new class file
+using _14CSharpBasics.Classes;
+
+namespace _14CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new double[] { 1, 2, 3, 6, 7 };
+            var result = simpleMath.Add(numbers);
+            Console.WriteLine(result);
+
+            //Object Definition -> this is how you refer to a class
+            BankAccount bankAccount = new BankAccount();
+            Console.WriteLine(bankAccount.Balance);
+            bankAccount.AddToBalance(134);
+            Console.WriteLine(bankAccount.Balance);
+
+            BankAccount bankAccount2 = new BankAccount(300);
+            Console.WriteLine(bankAccount2.Balance);
+            bankAccount2.AddToBalance(134);
+            Console.WriteLine(bankAccount2.Balance);
+
+            ChildBankAccount childBankAccount = new ChildBankAccount();
+            Console.WriteLine(childBankAccount.Balance);
+            childBankAccount.AddToBalance(134);
+            Console.WriteLine(childBankAccount.Balance);
+
+        }
+    }
+    class simpleMath
+    {
+        public static double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public static double Add(double[] numbers)
+        {
+            double Result = 0;
+            foreach (double number in numbers)
+            {
+                Result += number;
+            }
+            return Result;
+
+        }
+    }
+}
